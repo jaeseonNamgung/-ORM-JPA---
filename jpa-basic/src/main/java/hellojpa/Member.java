592dd77e06adcc8cb0e25bbf461f1e03629415b8
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-14 16:12:20 +0900

    기본 키 TABLE 사용 예제

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -1,18 +1,19 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
-@SequenceGenerator(
-        name = "member_seq_generator",
-        sequenceName = "member_seq"
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ",
+        allocationSize = 1
 )
 public class Member {
 
     // 기본키 매핑
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     // 컬럼 매핑
