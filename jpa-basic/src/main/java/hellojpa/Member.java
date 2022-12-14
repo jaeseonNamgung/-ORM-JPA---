95fd27fe20168cb4f795e5c4450c5515d2e13c76
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-14 14:41:26 +0900

    DDL 사용법 및 설명

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -1,28 +1,41 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
+    // 컬럼 매핑
+    @Column(name = "name")
+    private String username;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private int age;
 
-    public String getName() {
-        return name;
-    }
+    // enum 타입 매핑
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public void setName(String name) {
-        this.name = name;
+    // 날짜 타입 매핑
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createedDatee;
+
+    // 날짜 타입 매핑
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModefiedDate;
+
+    // @Lob 데이터에 큰 데이터를 주고 싶을 때 사용
+    // BLOB, CLOB 미팽
+    @Lob 
+    private String description;
+
+    // DB에 매핑하고 싶지 않고 메모리에서만 사용하고 싶을 떄 사용
+    @Transient
+    private int temp;
+    public Member(){
+        
     }
 }
