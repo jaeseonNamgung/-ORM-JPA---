3bf02ba985ef4d1c116d822ea53ca808b747ce4d
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-14 15:14:50 +0900

    기본 키 SEQUENCE 사용 예제

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -4,11 +4,15 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "member_seq_generator",
+        sequenceName = "member_seq"
+)
 public class Member {
 
     // 기본키 매핑
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
 
     // 컬럼 매핑
@@ -17,5 +21,13 @@ public class Member {
     
     public Member(){
         
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
