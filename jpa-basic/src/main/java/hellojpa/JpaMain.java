b6372de52ad5cc56c3bca9146dce20039f472b18
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-19 09:41:49 +0900

    프록시의 특징 - 준영속 상태일 때의 문제 발생

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -25,29 +25,16 @@ public class JpaMain {
             entityManager.flush();
             entityManager.clear();
 
-            // 값이 같은 이유
-            // 1. 이미 영속성 컨테이너 1차 캐시에 값이 있기 때문에 불 필요하게 다른 곳에서
-            // 값을 가져올 필요가 없음
-            // 2. 같은 영속성 컨테이너에서 가져온 값은 항상 같은 타입을 보장 해줘야 한다.
-            // 이게 JPA 동작하는 메커니즘이다.
-
-            Member findMember = entityManager.find(Member.class, member.getId());
-            System.out.println("findMember: " + findMember.getClass());
-
             Member reMember = entityManager.getReference(Member.class, member.getId());
-            System.out.println("reMember: " + reMember.getClass());
-
-            // 반대로 프록시를 먼저 초기화 할 경우 두 객체는 모두 같은 프록시 타입이 된다.
-
-
-
-
-
-
+            // 영속성 컨텍스트의 도움을 받을 수 없는 준영속 상태일 때, 프록시를 초기화하면
+            // 문제 발생 예) entityManager.detach(), close(), clear()
+            entityManager.detach(reMember); // 준영속 상태
+            System.out.println("reMember.getUsername : " + reMember.getUsername());
 
             entityTransaction.commit();
         }catch (Exception e){
             entityTransaction.rollback();
+            e.printStackTrace();
         }finally{
             entityManager.close();
         }
