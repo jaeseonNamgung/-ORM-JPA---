342c426ecf4d09c7167e333bc4e09d1dbec67dd0
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-19 09:35:37 +0900

    프록시의 특징 - 같은 타입 보장

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -25,13 +25,24 @@ public class JpaMain {
             entityManager.flush();
             entityManager.clear();
 
-            // 프록시 객체 초기화
-            // 프록시 객체를 초기화 할 때는 쿼리문을 날리지 않는다.
-            Member findMember = entityManager.getReference(Member.class, member.getId());
+            // 값이 같은 이유
+            // 1. 이미 영속성 컨테이너 1차 캐시에 값이 있기 때문에 불 필요하게 다른 곳에서
+            // 값을 가져올 필요가 없음
+            // 2. 같은 영속성 컨테이너에서 가져온 값은 항상 같은 타입을 보장 해줘야 한다.
+            // 이게 JPA 동작하는 메커니즘이다.
 
-            // findMember.getUsername() 처럼 값을 꺼낼때 영속성 컨테이너를 통해 DB와 연결되어
-            // 쿼리문을 통해 값을 얻어 올 수 있다.
-            System.out.println("findMember.getName = " + findMember.getUsername());
+            Member findMember = entityManager.find(Member.class, member.getId());
+            System.out.println("findMember: " + findMember.getClass());
+
+            Member reMember = entityManager.getReference(Member.class, member.getId());
+            System.out.println("reMember: " + reMember.getClass());
+
+            // 반대로 프록시를 먼저 초기화 할 경우 두 객체는 모두 같은 프록시 타입이 된다.
+
+
+
+
+
 
 
             entityTransaction.commit();
