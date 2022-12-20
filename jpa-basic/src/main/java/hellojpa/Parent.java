c435ce81c5cd145cb8317fd512ff310f3f92b328
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-20 17:09:19 +0900

    CASCADE 구현 코드 CascadeType.REMOVE

--- jpa-basic/src/main/java/hellojpa/Parent.java
+++ jpa-basic/src/main/java/hellojpa/Parent.java
@@ -17,7 +17,9 @@ public class Parent {
     // 예: 부모 엔티티를 저장할 때 자식 엔티티도 함께 저장
     // 주의: 영속성 전이는 연관관계를 매핑하는 것과 아무 관련이 없음
     // 엔티티를 영속화할 때 연관된 엔티티도 함께 영속화하는 편리함을 제공할 뿐
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+
+    // CascadeType.REMOVE 부모 엔티티(Team)에서 관련된 자식 엔티티(Member)를 제거하는 경우
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.REMOVE)
     List<Child> childList  = new ArrayList<>();
 
     // 연관 관계 편의 메서드
