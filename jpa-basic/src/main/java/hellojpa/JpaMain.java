3839f7dcbcefe5ad1334b27a304cc9564f260563
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-21 15:03:54 +0900

    값타입 SideEffect 상황
    같은 메모리를 참조하는 객체는 하나의 객체가 값을 변경해도 같은 메모리를 참조하는 모든 객체가 값이 변경되는 SideEffect가 발생하게 된다.

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -21,11 +21,19 @@ public class JpaMain {
 
         try {
 
-            Member member =  new Member();
-            member.setHomeAddress(new Address("a", "aa", "aaa"));
-            member.setWorkAddress(new Address("b", "bb", "bbb"));
-            member.setPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
-            entityManager.persist(member);
+            Address address = new Address("city", "street", "zipcode");
+
+            Member member1 =  new Member();
+            member1.setHomeAddress(address);
+
+            Member member2 =  new Member();
+            member2.setHomeAddress(address);
+            // 같은 메모리를 참조하는 객체는 하나의 객체가 값을 변경해도
+            // 같은 메모리를 참조하는 모든 객체가 값이 변경되는 SideEffect가
+            // 발생하게 된다.
+            member2.getHomeAddress().setCity("city2");
+            entityManager.persist(member1);
+            entityManager.persist(member2);
 
 
             entityTransaction.commit();
