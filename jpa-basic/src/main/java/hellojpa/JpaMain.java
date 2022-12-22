d56ad87870fe9105742ceca2db1660dcd47fab21
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-22 10:17:29 +0900

    컬렉션 조회 코드 구현
    - 컬렉션은 지연 로딩이 기본 값이다.

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -9,6 +9,7 @@ import javax.persistence.Persistence;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -33,8 +34,31 @@ public class JpaMain {
 
             member.getAddressHistory().add(new Address("old1", "street", "zipcode"));
             member.getAddressHistory().add(new Address("old2", "street", "zipcode"));
-
             entityManager.persist(member);
+
+            entityManager.flush();
+            entityManager.clear();
+
+            // 조회
+            // member 만 조회 할 경우 컬렉션 값 타입은 조회 되지 않는다.
+            // 왜냐하면 컬렉션 값 타입은 지연 로딩이 기본 값이기 때문이다.
+            System.out.println("==========조회==============");
+            Member findMember = entityManager.find(Member.class, member.getId());
+
+            // 컬렉션을 조회 했을 때 비로서 컬렉션 테이블도 select 된다.
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address value : addressHistory) {
+                System.out.println("getCity = " + value.getCity());
+                System.out.println("getStreet = " + value.getStreet());
+                System.out.println("getZipcode = " + value.getZipcode());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String value: favoriteFoods) {
+                System.out.println("favoriteFoods = " + value);
+            }
+
+
 
 
             entityTransaction.commit();
