fe82dbbbf66860bf4b08432f9d21dd11b6ca6d62
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-22 10:16:47 +0900

    컬렉션 구현 쿼리를 좀 더 쉽게 보기 위해 코드 정리

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -20,19 +20,8 @@ public class Member{
     @Column(name = "USERNAME")
     private String username;
 
-    // 1대N 양방향 연관관계 방법
-    // fetch = FetchType.EAGER : 즉시 로딩
-    // Member와 Team 중에 Member와 team이 90% 이상 같이 사용될 경우
-    // 즉시 로딩을 사용
-    // 즉시 로딩을 사용될 경우 Member와 Team이 조인으로 쿼리를 한번에 조회 된다.
-    // 실무에서 절대 사용하면 안된다. 무조건 지연 로딩으로 구현해야 한다.
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
 
     // 사용할 임베디드 타입을 지정
-    @Embedded
-    private Period period;
     @Embedded
     private Address homeAddress;
     
@@ -61,22 +50,6 @@ public class Member{
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-    public Period getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(Period period) {
-        this.period = period;
     }
 
     public Address getHomeAddress() {
