07e6c5b5909b377ae77da9ec608556ecddebbd38
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-26 09:12:09 +0900

    TypeQuery, Query
    - TypeQuery: 반환 타입이 명확할 때 사용
    - Qeury: 반환 타입이 명확하지 않을 때 사용

--- jpa-basic/src/main/java/jpql/JpaMain.java
+++ jpa-basic/src/main/java/jpql/JpaMain.java
@@ -1,9 +1,6 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -13,14 +10,26 @@ public class JpaMain {
 
         EntityManager em = ef.createEntityManager();
         EntityTransaction et=  em.getTransaction();
+        et.begin();
 
         try {
 
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(26);
+            em.persist(member);
+
+            // TypedQuery: 반환 타입이 명확할 때 사용
+            TypedQuery<Member> query =  em.createQuery("select m from Member m", Member.class);
+
+            // Query:  반환 타입이 명확하지 않을 때 사용
+            Query query1 = em.createQuery("select m.username, m.age from Member m");
 
             et.commit();
 
         }catch (Exception e){
             et.rollback();
+            e.printStackTrace();
         }
         finally {
             em.close();
