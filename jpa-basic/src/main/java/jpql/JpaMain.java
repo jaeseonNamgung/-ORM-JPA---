154a36f07e8182610775950a6c7ae4fa9fa2c3e3
jaeseonNamgung <sunnamgung8@naver.com>
2022-12-30 15:38:49 +0900

    엔티티 직접 사용 구현 코드

--- jpa-basic/src/main/java/jpql/JpaMain.java
+++ jpa-basic/src/main/java/jpql/JpaMain.java
@@ -15,52 +15,29 @@ public class JpaMain {
 
         try {
 
-            Team teamA = new Team();
-            teamA.setName("teamA");
-            Team teamB = new Team();
-            teamB.setName("teamB");
+           Team team = new Team();
+           team.setName("team");
 
-            Member memberA = new Member();
-            memberA.setUsername("회원1");
-            memberA.changeTeam(teamA);
-            em.persist(memberA);
-            Member memberB = new Member();
-            memberB.setUsername("회원2");
-            memberB.changeTeam(teamA);
-            em.persist(memberB);
-            Member memberC = new Member();
-            memberC.setUsername("회원3");
-            memberC.changeTeam(teamB);
-            em.persist(memberC);
+           Member member = new Member();
+           member.setUsername("member");
+           member.setAge(26);
+           member.changeTeam(team);
+           em.persist(member);
 
-            em.flush();
-            em.clear();
+           // where m = :member 엔티티 직접 사용
+            // where m.id = memberId 와 같다
+            // 실행된 SQL : select m.* from Member m where m.id = ?
 
-            // - 페치 조인 대상에는 별칭을 줄 수 없다. (페치 조인은 조인할 모든 값을 다 가져와야 된다.)
-            // 예) select m from Member m join fetch m.team as t
-            // - 둘 이상의 컬렉션은 페치 조인 할 수 없다.
-            // - 컬렉션을 페치 조인하면 페이징 API(setFirstResult, setMaxResult)를 사용할 수 없다.
-            //   1. 일대일, 다대일 같은 단일 값 연관 필드들은 페치 조인해도 페이징 가능
-            //   2. 하이버네이트는 경고 로그를 남기고 메모리에서 페이징(매우 위험)
-            //      일대다 에서 페이징 API 사용 방법
-            //          1. 다대일에서 페이징 API를 사용
-            //          2. @BatchSize 어노테이션 사용 (일대일 연관 매핑된 클래스에서 사용 예) team class)
-            //          글로버 셋팅으로 설정 가능 ( 더 많이 사용 )
-            //          <property name="hibernate.default_batch_fetch_size" value="100"/>
-            List<Team> teamList = em.createQuery("select t from Team t", Team.class)
-                            .setFirstResult(0)
-                            .setMaxResults(2)
-                            .getResultList();
-            for (Team t: teamList) {
-                System.out.println("fetch join(collection) team: " + t.getName() + "|memberss: " +
-                        t.getMemberList().size());
-                for (Member m : t.getMemberList()){
-                    System.out.println("member: " + m.getUsername());
-                }
-            }
+           Member findMember = em.createQuery("select m from Member m where m = :member", Member.class)
+                           .setParameter("member", member)
+                                   .getSingleResult();
+            System.out.println("result: " + findMember);
 
-
-
+            // 엔티티 직접 사용 - 외래 키 값
+            findMember = em.createQuery("select m from Member m where m.team = :team", Member.class)
+                            .setParameter("team", team)
+                                    .getSingleResult();
+            System.out.println("외래 키 값 result: " + findMember);
 
             et.commit();
 
